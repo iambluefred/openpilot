572aa941f2a98d0fe128846a3866204f612cd276
Comma Device <device@comma.ai>
2022-05-23 11:55:55 +0000

    EnableSpeed = -1.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -193,7 +193,7 @@ class CarInterface(object):
 
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter.
-    ret.minEnableSpeed = -1. if (stop_and_go or ret.enableGasInterceptor) else 19. * CV.MPH_TO_MS
+    ret.minEnableSpeed = -1.
 
     centerToRear = ret.wheelbase - ret.centerToFront
     # TODO: get actual value, for now starting with reasonable value for
